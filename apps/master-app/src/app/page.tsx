c98f99d285e839349ee7511d953124073925affe
7wanaio <1296032384@qq.com>
2024-01-27 13:54:44 +0800

    feat(basic project): create the basic project of the project and globally configure tailwindcss

---
+++ apps/master-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+     <div>hello master-app!</div>
+    </main>
+  );
+}
