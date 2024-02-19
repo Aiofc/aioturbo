7ea53bbdaa8ea9f570e48748ad064c57df0f3019
18577447378 <dadg39@qq.com>
2024-02-19 16:13:03 +0800

    feat(master-app): login

--- apps/master-app/next.config.js
+++ apps/master-app/next.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/gateway/:path*",
+        destination: "http://127.0.0.1:9999/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
---
+++ apps/master-app/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Password from "../../components/login/password";
+
+function Login() {
+  return (
+    <div>
+      <Password />
+    </div>
+  );
+}
+
+export default Login;
